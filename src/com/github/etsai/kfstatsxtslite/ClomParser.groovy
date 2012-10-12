f933d44977deb08310483a9d56dceeaca18ce28b
etsai <thegod0fde4th@gmail.com>
2012-10-11 22:44:52 -0700

    Setup netbeans files for the project

---
+++ src/com/github/etsai/kfstatsxtslite/ClomParser.groovy
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.github.etsai.kfstatsxtslite
+
+/**
+ * Parses command line options
+ * @author etsai
+ */
+public class ClomParser {
+
+}
+
---
+++ src/com/github/etsai/kfstatsxtslite/StatListenerMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.etsai.kfstatsxtslite;
+
+/**
+ * Main entry point for the UDP listener
+ * @author etsai
+ */
+public class StatListenerMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
