5f01b4d14248ef4b901c9b1a45f09e3a30fd5e59
etsai <thegod0fde4th@gmail.com>
2012-10-15 03:40:55 -0700

    Fixed bugs with the stored procedures
    Having columns and parameters with the same name is annoying

--- kfstatsx.sql
+++ kfstatsx.sql
@@ -14,15 +14,15 @@ CREATE  TABLE `records` (
   `disconnects` SMALLINT UNSIGNED NOT NULL DEFAULT 0 ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `steamID64_UNIQUE` (`steamID64` ASC) )
-COMMENT = 'Players\' W/L/D records';
+COMMENT = 'Players\' win, loss, and disconnect records';
 
 CREATE  TABLE `aggregate` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `stat` VARCHAR(45) NOT NULL ,
   `value` BIGINT UNSIGNED NOT NULL DEFAULT 0 ,
-  `group` VARCHAR(45) NOT NULL ,
+  `category` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `stat_group_UNIQUE` USING HASH (`stat` ASC, `group` ASC))
+  UNIQUE INDEX `stat_category_UNIQUE` USING HASH (`stat` ASC, `category` ASC))
 COMMENT = 'Stores the sum of each stat over all players';
 
 CREATE  TABLE `difficulties` (
@@ -51,58 +51,47 @@ CREATE  TABLE `player` (
   `steamID64` VARCHAR(45) NOT NULL ,
   `stat` VARCHAR(45) NOT NULL ,
   `value` INT UNSIGNED NOT NULL DEFAULT 0 ,
-  `group` VARCHAR(45) NOT NULL ,
+  `category` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `steamID64_stat_group_UNIQUE` USING HASH (`steamID64` ASC, `group` ASC, `stat` ASC)) 
+  UNIQUE INDEX `steamID64_category_stat_UNIQUE` USING HASH (`steamID64` ASC, `category` ASC, `stat` ASC)) 
 COMMENT = 'Stores individual player statistics';
 
-DROP procedure IF EXISTS `update_difficulty_and_level`;
 DELIMITER $$
 CREATE PROCEDURE `update_difficulty_and_level` 
     (diffname VARCHAR(45), length VARCHAR(45), levelname VARCHAR(45), 
-wins SMALLINT UNSIGNED, losses SMALLINT UNSIGNED, wave INT UNSIGNED, time INT UNSIGNED)
+winDelta SMALLINT UNSIGNED, lossDelta SMALLINT UNSIGNED, waveDelta INT UNSIGNED, timeDelta INT UNSIGNED)
 BEGIN
-INSERT INTO difficulties values (NULL, diffname, length, wins, losses, wave, time)
-    ON DUPLICATE KEY UPDATE `wins`= `wins` + wins, `losses`= `losses` + losses, 
-    `wave`= `wave` + wave, `time`= `time` + time;
+INSERT INTO difficulties values (NULL, diffname, length, winDelta, lossDelta, waveDelta, timeDelta)
+    ON DUPLICATE KEY UPDATE wins= wins + winDelta, losses= losses + lossDelta, 
+    wave= wave + waveDelta, time= time + timeDelta;
 
-INSERT INTO levels values (NULL, levelname, wins, losses, time)
-    ON DUPLICATE KEY UPDATE  `wins`= `wins` + wins, `losses`= `losses` + losses,
-    `time`= `time` + time;
+INSERT INTO levels values (NULL, levelname, winDelta, lossDelta, timeDelta)
+    ON DUPLICATE KEY UPDATE  wins= wins + winDelta, losses= losses + lossDelta,
+    time= time + timeDelta;
 END$$
-DELIMITER ;
 
-DROP procedure IF EXISTS `update_player_aggregate`;
-DELIMITER $$
 CREATE PROCEDURE `update_player_aggregate` 
-    (steamID64 VARCHAR(45), stat VARCHAR(45), offset INT UNSIGNED, statgroup VARCHAR(45))
+    (steamID64 VARCHAR(45), stat VARCHAR(45), offset INT UNSIGNED, category VARCHAR(45))
 BEGIN
-INSERT INTO player values (NULL, steamID64, stat, offset, statgroup)
+INSERT INTO player values (NULL, steamID64, stat, offset, category)
     ON DUPLICATE KEY UPDATE value= value + offset;
 
-INSERT INTO aggregate values (NULL, stat, offset, statgroup)
+INSERT INTO aggregate values (NULL, stat, offset, category)
     ON DUPLICATE KEY UPDATE value= value + offset;
 END$$
-DELIMITER ;
 
-DROP procedure IF EXISTS `update_record`;
-DELIMITER $$
 CREATE PROCEDURE `update_record` 
-    (steamID64 VARCHAR(45), wins SMALLINT UNSIGNED, losses SMALLINT UNSIGNED,
-    disconnects SMALLINT UNSIGNED)
+    (steamID64 VARCHAR(45), winDelta SMALLINT UNSIGNED, lossDelta SMALLINT UNSIGNED,
+    disconnectDelta SMALLINT UNSIGNED)
 BEGIN
-INSERT INTO records VALUES(NULL, steamID64, wins, losses, disconnects)
-    ON DUPLICATE KEY UPDATE `wins`= `wins` + wins, `losses`= `losses` + losses,
-    `disconnects`= `disconnects` + disconnects;
+INSERT INTO records VALUES (NULL, steamID64, winDelta, lossDelta, disconnectDelta)
+    ON DUPLICATE KEY UPDATE wins= wins + winDelta, losses= losses + lossDelta,
+    disconnects= disconnects + disconnectDelta;
 END$$
-DELIMITER ;
 
-DROP procedure IF EXISTS `update_deaths`;
-DELIMITER $$
 CREATE PROCEDURE `update_deaths` (name VARCHAR(45), offset SMALLINT UNSIGNED)
 BEGIN
 INSERT INTO deaths VALUES (NULL, name, offset)
     ON DUPLICATE KEY UPDATE count= count + offset;
-
 END$$
 DELIMITER ;
